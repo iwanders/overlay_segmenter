3b9ac02a2f5218d896f10310ce205f74a961f4bc
Ivor Wanders <ivor@iwanders.net>
2026-03-26 18:47:46 -0400

    Shuffle, swap license.

--- torch_stable/build.rs
+++ torch_stable/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let lib_path = "../../train/.venv/lib/python3.13/site-packages/torch/lib";
+    let lib_path = "../train/.venv/lib/python3.13/site-packages/torch/lib";
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search={lib_path}");
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_path);
