a753e68b9c916d81997b9f12a4a47ed3702e2130
Ivor Wanders <ivor@iwanders.net>
2026-07-18 18:20:39 -0400

    Add commandline parsing.

--- overlay_segmenter/examples/inference_on_files.rs
+++ overlay_segmenter/examples/inference_on_files.rs
@@ -1,37 +1,52 @@
-use anyhow::bail;
 use flash_powder as fp;
 use flash_powder::Tensor;
 use flash_powder::prelude::*;
 use flash_powder_image::prelude::*;
 use overlay_segmenter::common_setup;
+
+use clap::Parser;
+
+use std::path::PathBuf;
+
+/// Run inference on files.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the safetensors file.
+    #[arg(short, long)]
+    model: PathBuf,
+
+    /// Paths to the input images.
+    #[arg()]
+    files: Vec<PathBuf>,
+
+    /// Output directory, default is to output adjacent to the images.
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+
+    /// Write values
+    #[arg(short, long)]
+    write_values: bool,
+}
+
 pub fn main() -> Result<(), anyhow::Error> {
-    use std::path::PathBuf;
+    let args = Args::parse();
 
-    let args = std::env::args().collect::<Vec<String>>();
-
-    let safetensors_path = if let Some(path) = args.get(1) {
-        path.to_owned()
-    } else {
-        bail!("missing safetensors argument")
-    };
-    // Verify weights exist, if not give a nice warning.
-    let weights = PathBuf::from(safetensors_path);
-
-    let (unet, device, palette) = common_setup(&weights)?;
-    const COLOR_MASK_OUTPUT: bool = true;
+    let (unet, device, palette) = common_setup(&args.model)?;
 
     let palette = palette.to(&device.into())?;
     // Iterate over the input arguments and run the network.
-    for argument in std::env::args().skip(2) {
-        let path = std::path::PathBuf::from(&argument);
-        if argument.contains("_mask.png")
-            || argument.contains("_values.png")
-            || argument.contains("_batch.png")
-        {
+    for path in args.files.iter() {
+        let stem = path
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .expect("files passed should have a stem, else its a dir");
+        if stem.contains("_mask") || stem.contains("_values") || stem.contains("_batch") {
             // println!("  Ignoring {argument:?} because it looks like our output");
             continue;
         }
-        let img = Tensor::read_image(&argument)?.image_floatify(&device.into())?;
+        let img = Tensor::read_image(&path)?.image_floatify(&device.into())?;
         let channels_stacked = img.to(&unet.dtype().into())?;
         let dimension = (.., 64..(896 + 64), 128..1792); // 1664x832
         let indexed = channels_stacked.i(dimension.clone())?;
@@ -48,34 +63,39 @@ pub fn main() -> Result<(), anyhow::Error> {
             .i_mut(dimension)?
             .copy_from_tensor(&r.squeeze()?)?;
         let duration = (std::time::Instant::now() - start).as_secs_f64();
-        println!("{argument}: {duration:.2}s"); // First 0.29s, subseq 0.18
+        println!("{path:?}: {duration:.2}s"); // First 0.29s, subseq 0.18
 
-        let img = if COLOR_MASK_OUTPUT {
-            let pixel_index = mask_image.argmax(Some(0), Some(true))?;
-            let color_per_pixel = palette
-                .index_tensor(&[pixel_index])?
-                .squeeze()?
-                .to_owned()?;
+        let output_path = args
+            .output
+            .as_ref()
+            .cloned()
+            .unwrap_or(path.parent().unwrap().to_owned());
 
-            //img = tensor_to_image(&color_per_pixel.ten()?)?;
-            let color_per_pixel = color_per_pixel.to(&fp::Device::CPU.into())?;
-            let color_per_pixel = color_per_pixel.permute(&[2, 0, 1])?.contiguous()?;
-            color_per_pixel.to_dynamic_image()?
-        } else {
-            let t255: Tensor = 255.try_into()?;
-            let max = mask_image.argmax(Some(0), Some(true))?.mul(&t255)?;
-            let max_squeezed = max.squeeze()?;
-            // img = tensor_to_image(&max_squeezed.ten()?)?;
-            let max_squeezed = max_squeezed.to(&fp::Device::CPU.into())?;
-            max_squeezed.to_dynamic_image()?
-        };
+        println!("output_path: {output_path:?}");
+
+        // Next apply the color mask.
+
+        let pixel_index = mask_image.argmax(Some(0), Some(true))?;
+        let color_per_pixel = palette
+            .index_tensor(&[pixel_index])?
+            .squeeze()?
+            .to_owned()?;
+
+        //img = tensor_to_image(&color_per_pixel.ten()?)?;
+        let color_per_pixel = color_per_pixel.to(&fp::Device::CPU.into())?;
+        let color_per_pixel = color_per_pixel.permute(&[2, 0, 1])?.contiguous()?;
+        let img = color_per_pixel.to_dynamic_image()?;
 
         let stem = path.file_stem().unwrap().display();
-        img.save(format!("/tmp/{}_mask.png", stem))?;
-        // Lets also make value, to do that, we make mask_image from [C, H, W] into [C, 1, H, W].
-        let batch_of_values = mask_image.unsqueeze(1)?;
-        let batch_of_values_normalized = batch_of_values.image_scale_to_domain()?;
-        batch_of_values_normalized.save_image(format!("/tmp/{}_values.png", stem))?;
+        img.save(output_path.join(&format!("{}_mask.png", stem)))?;
+
+        if args.write_values {
+            // Lets also make value, to do that, we make mask_image from [C, H, W] into [C, 1, H, W].
+            let batch_of_values = mask_image.unsqueeze(1)?;
+            let batch_of_values_normalized = batch_of_values.image_scale_to_domain()?;
+            batch_of_values_normalized
+                .save_image(output_path.join(&format!("{}_values.png", stem)))?;
+        }
     }
 
     Ok(())
