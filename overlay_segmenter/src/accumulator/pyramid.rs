3f0d1424fefe9b368fb0a7eee0d350efbd183d05
Ivor Wanders <ivor@iwanders.net>
2026-07-22 07:39:55 -0400

    Something like this.

--- overlay_segmenter/src/accumulator/pyramid.rs
+++ overlay_segmenter/src/accumulator/pyramid.rs
@@ -6,7 +6,7 @@ use flash_powder_image::prelude::*;
 
 #[derive(Debug, Clone)]
 struct Layer {
-    scale: usize,
+    scale: isize,
     data: Tensor,
 }
 
@@ -63,14 +63,7 @@ impl Pyramid {
 
         debug_dir: Option<(&std::path::Path, &str)>,
     ) -> StableTorchResult<(isize, isize)> {
-        #[derive(Debug)]
-        struct NextLayerInfo {
-            grid: GridOverlay,
-            base_id: GridId,
-            other_id: GridId,
-            pos: (isize, isize),
-        }
-        let mut current_position: Option<NextLayerInfo> = None;
+        let mut current_full_res_pos: (isize, isize) = (0, 0);
         // Go through the pyramids in reverse order, from small images to large.
         for (layer, (b, o)) in self
             .layers
@@ -79,6 +72,9 @@ impl Pyramid {
             .zip(other.layers.iter().rev())
             .enumerate()
         {
+            println!();
+            println!("Global pos {current_full_res_pos:?}");
+
             // Layer 0 is a bit special, since there we just pad on all sides with the entire image size >_<
             // All other layers we only really need to do 4 left and right? which we can just do by chopping the borders
             // off the kernel.
@@ -100,20 +96,30 @@ impl Pyramid {
                     ..Default::default()
                 }
             };
+            let offset;
             let (base_img, other_img) = if layer == 0 {
+                offset = (0isize, 0isize);
                 (base_img, other_img)
             } else {
-                // Get the overlapping sections of base_img and other_img.
-                let c = current_position.as_ref().unwrap();
-                let (base_overlap_x, base_overlap_y) = c.grid.overlap_irange(c.base_id);
-                let (other_overlap_x, other_overlap_y) = c.grid.overlap_irange(c.other_id);
+                let mut grid = GridOverlay::new();
+                let base_id = grid.add_tensor(
+                    &base_img,
+                    (
+                        (current_full_res_pos.0 / (b.scale as isize)),
+                        (current_full_res_pos.1 / (b.scale as isize)),
+                    ),
+                );
+                let other_id = grid.add_tensor(&other_img, (0, 0));
+                let (base_overlap_x, base_overlap_y) = grid.overlap_irange(base_id);
+                let (other_overlap_x, other_overlap_y) = grid.overlap_irange(other_id);
+                offset = (grid.overlap().0.x, grid.overlap().0.y);
 
                 // That's in the previous dimension though, so next we need to scale that by two, and then we can slice.
 
-                let base_overlap_x = base_overlap_x.start * 2..base_overlap_x.end * 2;
-                let base_overlap_y = base_overlap_y.start * 2..base_overlap_y.end * 2;
-                let other_overlap_x = other_overlap_x.start * 2..other_overlap_x.end * 2;
-                let other_overlap_y = other_overlap_y.start * 2..other_overlap_y.end * 2;
+                let base_overlap_x = base_overlap_x.start..base_overlap_x.end;
+                let base_overlap_y = base_overlap_y.start..base_overlap_y.end;
+                let other_overlap_x = other_overlap_x.start..other_overlap_x.end;
+                let other_overlap_y = other_overlap_y.start..other_overlap_y.end;
 
                 let base_img = base_img.i((.., .., base_overlap_y, base_overlap_x))?;
                 let other_img = other_img.i((.., .., other_overlap_y, other_overlap_x))?;
@@ -135,6 +141,29 @@ impl Pyramid {
                 other_img.image_scale_to_domain()?.save_image(
                     &output_dir.join(&format!("{output_prefix}_level_{layer}_kernel.png")),
                 )?;
+
+                {
+                    let h = base_img.isize(-2);
+                    let w = base_img.isize(-1);
+                    let options = flash_powder::factory::TensorOptions {
+                        dtype: Some(base_img.dtype()),
+                        device: Some(base_img.device()),
+                        ..Default::default()
+                    };
+                    let mut canvas: Tensor = Tensor::zeros(&[3, h, w], &options)?; // base_channels
+                    // Blit the base;
+                    canvas
+                        .i_mut((0, .., ..))?
+                        .copy_from_tensor(&base_img.squeeze()?)?;
+                    // And blit the other part
+                    canvas
+                        .i_mut((1, .., ..))?
+                        .copy_from_tensor(&other_img.squeeze()?)?;
+
+                    canvas.image_scale_to_domain()?.save_image(
+                        &output_dir.join(&format!("{output_prefix}_aligned_pre_{layer}.png")),
+                    )?;
+                }
             }
 
             let (values, indices) = conv2.flatten(0, None)?.topk(1, &Default::default())?;
@@ -152,49 +181,69 @@ impl Pyramid {
             println!("shape conv2: {:?}", conv2.shape());
             //println!("conv2: {:?}", conv2);
             println!("values: {:?} {:?} {value}", values.shape(), values);
-            println!("indices: {:?} peak at {x},{y}", indices);
-            if current_position.is_none() {
+
+            if layer == 0 {
+                current_full_res_pos = (
+                    (offset.0 + conv2.isize(-1) as isize / 2 - x as isize) * b.scale as isize,
+                    (offset.1 + conv2.isize(-2) as isize / 2 - y as isize) * b.scale as isize,
+                );
+            } else {
+                current_full_res_pos.0 +=
+                    (conv2.isize(-1) as isize / 2 - x as isize) * b.scale as isize;
+                current_full_res_pos.1 +=
+                    (conv2.isize(-2) as isize / 2 - y as isize) * b.scale as isize;
+            }
+            println!(
+                "indices: {:?} peak at {x},{y}  at scale {}  offset {offset:?}  current_full_res_pos: {current_full_res_pos:?}",
+                indices, b.scale
+            );
+
+            if let Some((output_dir, output_prefix)) = debug_dir {
                 let mut grid = GridOverlay::new();
+
+                let base_img = self
+                    .layers
+                    .last()
+                    .as_ref()
+                    .map(|z| z.data.ten().unwrap())
+                    .unwrap();
+                let other_img = self
+                    .layers
+                    .last()
+                    .as_ref()
+                    .map(|z| z.data.ten().unwrap())
+                    .unwrap();
+                let scale = 1;
                 let base_id = grid.add_tensor(&base_img, (0, 0));
                 let other_id = grid.add_tensor(
                     &other_img,
                     (
-                        x as isize - options.padding.1 as isize,
-                        y as isize - options.padding.0 as isize,
+                        (current_full_res_pos.0 / b.scale),
+                        (current_full_res_pos.1 / b.scale),
                     ),
                 );
-                let pos = (x as _, y as _);
+                let full = grid.full_size();
+                println!("full: {full:?}");
+                let options = flash_powder::factory::TensorOptions {
+                    dtype: Some(base_img.dtype()),
+                    device: Some(base_img.device()),
+                    ..Default::default()
+                };
+                let mut canvas: Tensor = Tensor::zeros(&[3, full.1, full.0], &options)?; // base_channels
+                // Blit the base;
+                let (bx, by) = grid.full_grid_irange(base_id);
+                canvas
+                    .i_mut((0, by, bx))?
+                    .copy_from_tensor(&base_img.squeeze()?)?;
+                // And blit the other part
+                let (bx, by) = grid.full_grid_irange(other_id);
+                canvas
+                    .i_mut((1, by, bx))?
+                    .copy_from_tensor(&other_img.squeeze()?)?;
 
-                if let Some((output_dir, output_prefix)) = debug_dir {
-                    let full = grid.full_size();
-                    println!("full: {full:?}");
-                    let options = flash_powder::factory::TensorOptions {
-                        dtype: Some(base_img.dtype()),
-                        device: Some(base_img.device()),
-                        ..Default::default()
-                    };
-                    let mut canvas: Tensor = Tensor::zeros(&[3, full.1, full.0], &options)?; // base_channels
-                    // Blit the base;
-                    let (bx, by) = grid.full_grid_irange(base_id);
-                    canvas
-                        .i_mut((0, by, bx))?
-                        .copy_from_tensor(&base_img.squeeze()?)?;
-                    // And blit the other part
-                    let (bx, by) = grid.full_grid_irange(other_id);
-                    canvas
-                        .i_mut((1, by, bx))?
-                        .copy_from_tensor(&other_img.squeeze()?)?;
-
-                    canvas
-                        .image_scale_to_domain()?
-                        .save_image(&output_dir.join(&format!("{output_prefix}_aligned.png")))?;
-                }
-                current_position = Some(NextLayerInfo {
-                    grid,
-                    base_id,
-                    other_id,
-                    pos,
-                });
+                canvas.image_scale_to_domain()?.save_image(
+                    &output_dir.join(&format!("{output_prefix}_aligned_{layer}.png")),
+                )?;
             }
         }
 
